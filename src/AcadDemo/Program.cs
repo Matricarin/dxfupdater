a8dbe42b1e43e05e6c7e3efe5deead0bc6796ea4
Matricarin <matricarin@gmail.com>
2026-02-04 23:29:07 +0500

    new sample was added

--- src/AcadDemo/Program.cs
+++ src/AcadDemo/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace AcadDemo
+﻿using ACadSharp;
+using ACadSharp.Entities;
+using ACadSharp.IO;
+
+namespace AcadDemo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var path = @"Samples/2408.dxf";
+
+           var doc = DxfReader.Read(path, new DxfReaderConfiguration()
+            {
+                ClearCache = true,
+                CreateDefaults = true,
+                Failsafe = true,
+                KeepUnknownEntities = true,
+                KeepUnknownNonGraphicalObjects = true
+            });
+
+
+            var texts = doc.Entities.Where(entity => entity.ObjectType == ObjectType.TEXT).ToArray();
+
+            foreach (var entity in texts)
+            {
+                var text = entity as TextEntity;
+                
+                Console.WriteLine(text.Value);
+            }
+
+            Console.ReadLine();
         }
     }
 }
